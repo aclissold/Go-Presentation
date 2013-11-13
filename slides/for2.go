a3f933de4e64faba5c3be87ad27213c87f8821ba
Andrew Clissold <andrewclissold@gmail.com>
2013-11-13 15:13:23 -0500

    Fix second for loop example

--- slides/for2.go
+++ slides/for2.go
@@ -5,9 +5,10 @@ package main
 import "fmt"
 
 func main() {
-    // START OMIT
-    for i := 0; i < 10; i++ {
-        fmt.Println(i)
-    }
-    // END OMIT
+	// START OMIT
+	words := []string{"apple", "banana", "cantaloupe"}
+	for i, word := range words {
+		fmt.Println(word, "is located at index", i)
+	}
+	// END OMIT
 }
